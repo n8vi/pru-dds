b59b3422b306e034b596b3fcc18876b67eb58a12
Brad Tarratt, N8VI <n8vi@rfc791.ORG>
2014-02-20 06:29:29 -0500

    startdds.c simplification

--- startdds.c
+++ startdds.c
@@ -1,22 +1,6 @@
-// #include <stdio.h>
-// #include <sys/mman.h>
-// #include <fcntl.h>
-// #include <errno.h>
-// #include <unistd.h>
-// #include <string.h>
-// #include <math.h>
-// #include <stdlib.h>
-
 #include <prussdrv.h>
 #include <pruss_intc_mapping.h>
 #include "util.h"
-
-// #define PRU_NUM          0
-
-// #define DDR_BASEADDR     0x80000000
-// #define OFFSET_DDR       0x00001000
-
-#define PI (3.141592653585)
 
 int main (void)
 {

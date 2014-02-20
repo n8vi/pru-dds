12c94d746a23084c7e6df3692ff8395785a0246f
Brad Tarratt, N8VI <n8vi@rfc791.ORG>
2014-02-20 06:40:41 -0500

    Clean up sine.c

--- sine.c
+++ sine.c
@@ -1,63 +1,35 @@
 #include <stdio.h>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
-#include <string.h>
 #include <math.h>
-#include "config.h"
-
-#include <prussdrv.h>
-#include <pruss_intc_mapping.h>
-
-#define PRU_NUM          0
-
-#define DDR_BASEADDR     0x80000000
-#define OFFSET_DDR       0x00001000
+#include "util.h"
 
 #define PI (3.141592653585)
 
-static int loadwavetable(unsigned short pruNum)
+static int loadwavetable()
 {
   int i;
   double f;
-  unsigned char *pruDataMem;
+  unsigned char *table;
 
-  //Initialize pointer to PRU data memory
-  if (pruNum == 0)
-  { 
-    prussdrv_map_prumem (PRUSS0_PRU0_DATARAM, (void**)&pruDataMem);
-  } else if (pruNum == 1) { 
-    prussdrv_map_prumem (PRUSS0_PRU1_DATARAM, (void**)&pruDataMem);
+  table = wavetable();
+  if (table) {
+    for (i=0; i<TABLELEN; i++) {
+      f = sin(2*PI*i/TABLELEN);
+      f = 127.5*f+128;
+      table[i] = (unsigned int)f;
+      }
+    return 0;
+  } else {
+    return 1;
     }
-
-  for (i=0; i<TABLELEN; i++) {
-    f = sin(2*PI*i/TABLELEN);
-    f = 127.5*f+128;
-    pruDataMem[i] = (unsigned int)f;
-    }
-  return(0);
 }
-
   
 int main (void)
 {
-  unsigned int ret;
-  tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+  pruinit();
 
-  prussdrv_init ();
-
-  ret = prussdrv_open(PRU_EVTOUT_0);
-  if (ret) {
-    printf("prussdrv_open open failed\n");
-    return (ret);
+  if (loadwavetable()) {
+    printf("Error loading wavetable\n");
     }
-
-  prussdrv_pruintc_init(&pruss_intc_initdata);
-
-  // you can stop and restart the PRU if you want, but I think this looks neat
-
-  loadwavetable(PRU_NUM);
 
   return(0);
 }
--- util.h
+++ util.h
@@ -1,5 +1,7 @@
 #ifndef _UTIL_H
 
+#include "config.h"
+
 #define PRU_NUM          0
 #define DDR_BASEADDR     0x80000000
 #define OFFSET_DDR       0x00001000

9d59e854588388b9e46f9c05ce4a546e8775e0b8
Brad Tarratt, N8VI <n8vi@rfc791.ORG>
2014-02-19 22:43:00 -0500

    setfreq.c tested.  Removed setskip.c, setfreq.pl.

---
+++ util.h
@@ -0,0 +1,14 @@
+#ifndef _UTIL_H
+
+#define PRU_NUM          0
+#define DDR_BASEADDR     0x80000000
+#define OFFSET_DDR       0x00001000
+
+int setskip (double skiplenf);
+unsigned char *wavetable();
+
+int setfreq(double freq);
+int pruinit (void);
+
+
+#endif

4f6b25f14a0b7d07d64eba6aa86b629d76447c5c
Brad Tarratt, N8VI <n8vi@rfc791.ORG>
2014-03-02 09:39:07 -0500

    Added further comments in config.pl

--- config.pl
+++ config.pl
@@ -4,13 +4,16 @@ $tablebits = 12;
 # How many bits should be used to address the wavetable?
 # The number of samples in the wavetable will be two raised
 # to this power, maximum 13.  13=>8192, 12=>4096, etc
+# rerun startdds and one of the wavetable loaders after changing this
 
 $ddsbits = 8;
 # how many bits should the DDS output?
 # Choose 1 to 8 (perhaps more later)
+# run one of the wavetable loaders after changing this
 
 $cpucycles = 7;
 # How many CPU cycles in the DDS main loop? (see dds.p)
+# rerun startdds after changing this
 
 $clockrate = 200000000;
 # CPU cycles per second of the PRU.
@@ -18,6 +21,8 @@ $clockrate = 200000000;
 
 $prunum = 0;
 # Which PRU are we to run the DDS code on?
+# rerun startdds after changing this
 
 $realtimefreq = 1;
 # glitch-free frequency changes (in exchange for lower max frequency)
+# rerun startdds and setfreq after changing this

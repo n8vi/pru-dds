fe837185b9512d3541c57b92f6174ee47a9d58d5
Brad Tarratt, N8VI <n8vi@rfc791.ORG>
2014-03-08 22:32:07 -0500

    put licencey text in things

--- genfile.pl
+++ genfile.pl
@@ -1,5 +1,25 @@
 #!/usr/bin/perl
 
+# Copyright (c) 2014 Brad J. Tarratt
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 do 'config.pl';
 
 $tablelen = 1<<$tablebits;  # how many 8-bit samples in our table?
@@ -39,7 +59,9 @@ if ($ARGV[0] eq '_config_h') {
   $samprate = $clockrate / $cpucycles;
   $nyquist = $samprate / 2;
 
-  printf "\n\nCurrent configuration allows for $samprate samples per second (nyquist $nyquist)\n";
+  printf "\n\n * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n";
+  printf " * Current configuration allows for $samprate samples per second (nyquist $nyquist)  *\n";
+  printf " * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n\n";
 
   open($configh, ">config.h");
 
